7b783cd2b109daf434ecb02a7f31c2576552de27
Jeanty01 <jeantynassau@icloud.com>
2022-02-20 13:54:30 +0200

    First commit

---
+++ src/firebase.js
@@ -0,0 +1,8 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyBZaRk8IesEEA68JRiwoR9q6-24JDXv5oQ",
+  authDomain: "whatsapp-mern-b407e.firebaseapp.com",
+  projectId: "whatsapp-mern-b407e",
+  storageBucket: "whatsapp-mern-b407e.appspot.com",
+  messagingSenderId: "405403748161",
+  appId: "1:405403748161:web:eb45d9adb0a42c0c8843c0"
+};
